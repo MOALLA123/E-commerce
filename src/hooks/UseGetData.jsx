269873fbc5400d3876d71c30ae2f54eb81420c09
MOALLA123 <MOALLA123@example.com>
2024-09-25 02:29:37 +0300

    ff

--- src/hooks/UseGetData.jsx
+++ src/hooks/UseGetData.jsx
@@ -3,8 +3,6 @@ import { baseUrl } from "../Api/baseUrl";
 const UseGetData = async (url, params) => {
   const res = await baseUrl.get(url, params);
 
-  console.log(res, "getdata");
-
   return res;
 };
 export default UseGetData;
